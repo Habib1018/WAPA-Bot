b5e05343324fd453cf54d3a240a48f4d25bc1908
Habib1018 <proyectos.nom@gmail.com>
2025-03-27 21:28:49 -0600

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-import os
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "WAPA2025PODER")
-ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "DEFAULT_ACCESS_TOKEN")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "TU_ACCESS_TOKEN_DE_META")
 
 @app.route("/", methods=["GET"])
 def verify():
@@ -17,12 +17,6 @@ def verify():
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
-    print("Mensaje recibido:", data)
-    return "ok", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
-
     print("Mensaje recibido:", data)
     return "ok", 200
 
